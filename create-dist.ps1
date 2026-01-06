a6e4f7a0ac6fcc6c222b91a3bb7ef0c4e8222c4f
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-06 21:47:54 +0300

    build(scripts): add publish module step

--- create-dist.ps1
+++ create-dist.ps1
@@ -3,39 +3,47 @@ param (
     [string]$Prerelease = $null
 )
 
-$moduleName = "PSSemanticRelease"
-$distFolder = Join-Path $PSScriptRoot "dist"
-$masterPath = Join-Path $PSScriptRoot $moduleName
+try {
+    $moduleName = "PSSemanticRelease"
+    $distFolder = Join-Path $PSScriptRoot "dist"
+    $masterPath = Join-Path $PSScriptRoot $moduleName
 
-if (Test-Path $distFolder) {
-    Get-ChildItem -Path $distFolder -Recurse | Remove-Item -Recurse -Force
-}
-else {
-    New-Item -Path $distFolder -ItemType Directory | Out-Null
-}
+    if (Test-Path $distFolder) {
+        Get-ChildItem -Path $distFolder -Recurse | Remove-Item -Recurse -Force
+    }
+    else {
+        New-Item -Path $distFolder -ItemType Directory | Out-Null
+    }
 
-$distModuleFolder = "$distFolder/$moduleName"
+    $distModuleFolder = "$distFolder/$moduleName"
 
-New-Item -Path $distModuleFolder -ItemType Directory | Out-Null
+    New-Item -Path $distModuleFolder -ItemType Directory | Out-Null
 
-Get-ChildItem -Path $masterPath | Copy-Item -Destination $distModuleFolder -Recurse
+    Get-ChildItem -Path $masterPath | Copy-Item -Destination $distModuleFolder -Recurse
 
-$psd1Path = "$distModuleFolder/$moduleName.psd1"
+    $psd1Path = "$distModuleFolder/$moduleName.psd1"
 
-$names = (Get-ChildItem "$PSScriptRoot/$moduleName/public/*.ps1").BaseName
+    $names = (Get-ChildItem "$PSScriptRoot/$moduleName/public/*.ps1").BaseName
 
-New-ModuleManifest `
-    -Path $psd1Path `
-    -RootModule "$moduleName.psm1" `
-    -ModuleVersion $Version `
-    -Author $env:NUGET_PUBLISHER `
-    -FunctionsToExport $names `
-    -CmdletsToExport @() `
-    -Guid $env:NUGET_PACKAGE_GUID
+    New-ModuleManifest `
+        -Path $psd1Path `
+        -RootModule "$moduleName.psm1" `
+        -ModuleVersion $Version `
+        -Author $env:NUGET_PUBLISHER `
+        -FunctionsToExport $names `
+        -CmdletsToExport @() `
+        -Guid $env:NUGET_PACKAGE_GUID
     
-$psd1 = Get-Content $psd1Path -Raw
-$psd1 = $psd1 -replace "# ReleaseNotes = ''", "ReleaseNotes = '$Prerelease'"
+    $psd1 = Get-Content $psd1Path -Raw
+    $psd1 = $psd1 -replace "# ReleaseNotes = ''", "ReleaseNotes = '$Prerelease'"
 
-Set-Content $psd1Path -Value $psd1
+    Set-Content $psd1Path -Value $psd1
 
-Write-Host "[ps-semantic-release] $moduleName.psd1 successfully created."
+    Write-Host "$moduleName.psd1 successfully created."
+
+    Publish-Module -Path $distModuleFolder -NuGetApiKey $env:NUGET_API_KEY
+}
+catch {
+    throw $_
+}
+
--- publish-dist.ps1
+++
@@ -1,4 +0,0 @@
-$distFolder = Join-Path $PSScriptRoot "dist"
-$moduleFolder = Join-Path $distFolder "PSSemanticRelease"
-
-Publish-Module -Path $moduleFolder -NuGetApiKey $env:NUGET_API_KEY
