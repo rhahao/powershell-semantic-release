ae73577a93ff713139ca25a87ef608613c623ab8
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-06 19:00:12 +0300

    feat(module): include ci token names in exception

--- PSSemanticRelease/private/Confirm-EnvironmentCI.ps1
+++ PSSemanticRelease/private/Confirm-EnvironmentCI.ps1
@@ -1,12 +1,12 @@
 function Confirm-EnvironmentCI {
   if ($env:GITLAB_CI -eq "true") {
     if (-not $env:GITLAB_TOKEN -and -not $env:GL_TOKEN) {
-      throw "No GitLab token found in CI environment."
+      throw "No GitLab token (GITLAB_TOKEN or GL_TOKEN) found in CI environment."
     }
   }
   elseif ($env:GITHUB_ACTIONS -eq "true") {
     if (-not $env:GITHUB_TOKEN -and -not $env:GH_TOKEN) {
-      throw "No GitHub token found in CI environment."
+      throw "No GitHub token (GITHUB_TOKEN or GH_TOKEN) found in CI environment."
     }
   }
 }
