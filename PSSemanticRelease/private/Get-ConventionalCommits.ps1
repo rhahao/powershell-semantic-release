62565301f09b48d768c0a0dc6071ecc00f78f5eb
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-06 21:22:16 +0300

    fix(private): git fetch issue in Get-ConventionalCommits

--- PSSemanticRelease/private/Get-ConventionalCommits.ps1
+++ PSSemanticRelease/private/Get-ConventionalCommits.ps1
@@ -3,26 +3,20 @@ function Get-ConventionalCommits {
 
     $Branch = $context.Branch
 
-    if ($Branch) {
-        $localBranchExists = git show-ref --verify --quiet "refs/heads/$Branch"
-        if (-not $localBranchExists) {
-            git fetch origin "${Branch}:$Branch" --quiet
-        }
+    if ($Branch -and $Branch -ne 'HEAD') {
+        git fetch origin $Branch --quiet
     }
 
-    $ref = if ($Branch) { $Branch } else { 'HEAD' }
+    $ref = if ($Branch -and $Branch -ne 'HEAD') { "origin/$Branch" } else { 'HEAD' }
 
     $lastTag = git describe --tags --abbrev=0 $ref 2>$null
 
     $range = if ($lastTag) { "$lastTag..$ref" } else { $ref }
 
     $commits = [System.Collections.Generic.List[object]]::new()
-
     foreach ($line in git log $range --pretty=format:%s --reverse) {
         $commit = ConvertFrom-Commit $line
-        if ($commit) {
-            [void]$commits.Add($commit)
-        }
+        if ($commit) { [void]$commits.Add($commit) }
     }
 
     return ,$commits.ToArray()
