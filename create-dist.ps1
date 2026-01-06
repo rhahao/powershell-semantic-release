9a85d19c820ff0d626463a9808f7e99d1bad2bbc
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-07 00:38:46 +0300

    fix(scripts): sign all module files

--- create-dist.ps1
+++ create-dist.ps1
@@ -23,7 +23,6 @@ try {
     Get-ChildItem -Path $masterPath | Copy-Item -Destination $distModuleFolder -Recurse
 
     $psd1Path = "$distModuleFolder/$moduleName.psd1"
-    $psm1Path = "$distModuleFolder/$moduleName.psm1"
 
     $names = (Get-ChildItem "$PSScriptRoot/$moduleName/public/*.ps1").BaseName
 
@@ -48,11 +47,12 @@ try {
         $cert = New-SelfSignedCertificate -DnsName "$moduleName-Signing" -CertStoreLocation "Cert:\CurrentUser\My" -Type CodeSigningCert
         Write-Host "Created self-signed certificate: $($cert.Thumbprint)"
 
-        Set-AuthenticodeSignature -FilePath $psd1Path -Certificate $cert | Out-Null
-        Write-Host "$moduleName.psd1 module manifest file signed"
+        $files = Get-ChildItem -Path $distModuleFolder -File -Recurse
 
-        Set-AuthenticodeSignature -FilePath $psm1Path -Certificate $cert | Out-Null
-        Write-Host "$moduleName.psm1 module script file signed"
+        foreach ($file in $files) {
+            Set-AuthenticodeSignature -FilePath $file.FullName -Certificate $cert | Out-Null
+            Write-Host "$($file.Name) module manifest file signed"
+        }
     
         Publish-Module -Path $distModuleFolder -NuGetApiKey $env:NUGET_API_KEY
         Write-Host "$moduleName published successfully"
