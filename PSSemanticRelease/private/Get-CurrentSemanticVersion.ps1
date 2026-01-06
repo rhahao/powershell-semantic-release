e93139c72f390cca4f76771e2c56d58a0a6cc2e7
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-06 21:04:09 +0300

    chore(private): remove some comments

--- PSSemanticRelease/private/Get-CurrentSemanticVersion.ps1
+++ PSSemanticRelease/private/Get-CurrentSemanticVersion.ps1
@@ -4,31 +4,24 @@ function Get-CurrentSemanticVersion {
         $Branch = "HEAD"
     )
 
-    # 1️⃣ Fetch all tags
     git fetch --tags --quiet
 
     if ($context.Config.unify_tag) {
-        # Use the highest semantic version from all tags
         $lastTag = git tag --list | Sort-Object { [version]($_ -replace '^v', '') } -Descending | Select-Object -First 1
     }
     else {
-        # Determine the branch or fallback to HEAD
         if ($Branch -ne 'HEAD') {
-            # Check if branch exists locally
             $exists = git show-ref --verify --quiet "refs/heads/$Branch"
+
             if (-not $exists) {
-                & $context.Logger "Fetching branch '$Branch' from origin..."
-                git fetch origin "${Branch}:$Branch"
+                git fetch origin "${Branch}:$Branch" --quiet
             }
         }
 
-        # Use branch (or HEAD) to get latest tag
         $ref = if ($Branch) { $Branch } else { 'HEAD' }
 
-        # Get last tag reachable from ref
         $lastTag = git describe --tags --abbrev=0 $ref 2>$null
     }
-
-    # Return tag without leading v
+    
     return $lastTag -replace '^v', ''
 }
