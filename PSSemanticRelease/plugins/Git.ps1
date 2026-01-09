8acd2402bb9eb99065362499c7c951ec3a040c4c
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-09 14:46:33 +0300

    feat(plugins): add Prepare step for Git

--- PSSemanticRelease/plugins/Git.ps1
+++ PSSemanticRelease/plugins/Git.ps1
@@ -32,7 +32,46 @@ class Git {
 
         Add-ConsoleLog "Start step $step of plugin $typeName"
 
-        
+        $assets = , $this.Config.assets
+        $messageTemplate = $this.Config.message
+
+        $lists = @()
+        $modifiedFiles = Get-GitModifiedFiles
+
+        foreach ($file in $modifiedFiles) {
+            foreach ($pathRule in $assets) {
+                if ($file -like $pathRule) {
+                    $found = Get-Item -Path $file
+                    $lists += $found.FullName
+                    break
+                }
+            }
+        }
+
+        if ($lists.Count -eq 0) { 
+            Add-ConsoleLog "[Git] Cannot find files listed in assets config"
+            return
+        }
+
+        Add-ConsoleLog "[Git] Found $($lists.Count) file(s) to commit"
+
+        # Stage files
+        git add $lists 2>$null
+
+        git restore .
+        git restore --staged .
+
+        git add $lists 2>$null
+
+        $commitMessage = Expand-ContextString -context $this.Context -template $messageTemplate
+        $currentBranch = $this.Context.Repository.BranchCurrent
+
+        git commit -m $commitMessage --quiet
+        git push origin $currentBranch --quiet
+
+        $nextVersion = $this.Context.NextRelease.Version
+
+        Add-ConsoleLog "[Git] Prepared Git release: v${nextVersion}"
 
         Add-ConsoleLog "Completed step $step of plugin $typeName"
     }
--- PSSemanticRelease/private/GitHelpers.ps1
+++ PSSemanticRelease/private/GitHelpers.ps1
@@ -166,63 +166,6 @@ function Set-GitIdentity {
     }
 }
 
-function Push-GitAssets {
-    param($context)
-
-    $gitConfig = $Context.Config.git
-
-    if (-not $gitConfig) { return }
-
-    $assets = $gitConfig.assets
-    $messageTemplate = $gitConfig.message
-
-    if (-not $assets -or -not $messageTemplate) { return }
-
-    $assets = , $assets
-
-    if ($assets.Length -eq 0) { return }
-
-    $lists = @()
-
-    foreach ($asset in $assets) {
-        $found = Get-Item -Path $asset -ErrorAction SilentlyContinue
-
-        if (-not $found) { continue }
-
-        $lists += $found.FullName
-    }
-
-    if ($lists.Length -eq 0) { return }
-
-    $filesCount = 0
-
-    foreach ($asset in $assets) {
-        $path = Get-Item -Path $asset
-
-        if ($path.PSIsContainer) {
-            $filesCount += (Get-ChildItem -Path $asset -File -Recurse).Count
-        }
-        else {
-            $filesCount++
-        }
-    }
-
-    Add-ConsoleLog "Found $filesCount file(s) to commit"
-
-    # Stage files
-    git add $lists 2>$null
-
-    git restore .
-    git restore --staged .
-
-    git add $lists 2>$null
-
-    $commitMessage = Expand-ContextString -context $context -template $messageTemplate
-
-    git commit -m $commitMessage --quiet
-    git push origin $Context.Branch --quiet
-}
-
 function New-GitTag {
     param ($version)
 
@@ -292,4 +235,8 @@ function Get-NextSemanticVersion {
     }
 
     $context.NextRelease.Version = $nextVersion
+}
+
+function Get-GitModifiedFiles {
+    return git ls-files -m -o
 }
