8421fc0fd8e0ebe041b6af3cf9357bc793bc9346
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-10 22:53:16 +0300

    build(config): remove DryRun from publish step

--- publish-dist.ps1
+++ publish-dist.ps1
@@ -1,13 +1,7 @@
-param (
-    [string]$DryRun
-)
-
 $moduleName = 'PSSemanticRelease'
 $distRoot = Join-Path $PSScriptRoot 'dist'
 $distPath = Join-Path $distRoot $moduleName
 
-if ($DryRun -like "false") {
-    Write-Host "Publishing module to PSGallery"
-    Publish-Module  -Path $distPath -NuGetApiKey $env:NUGET_API_KEY -Repository PSGallery
-    Write-Host "Publish completed successfully"
-}
+Write-Host "Publishing module to PSGallery"
+Publish-Module  -Path $distPath -NuGetApiKey $env:NUGET_API_KEY -Repository PSGallery
+Write-Host "Publish completed successfully"
